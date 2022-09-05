38c3a928fda48863f92b26dd0e6bc00c3ad77eb4
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-09-05 20:56:46 -0300

    add table

---
+++ src/components/table/table.ts
@@ -0,0 +1,26 @@
+import { CellState } from '../../contants/cell-state-enum';
+
+export class Table {
+  private readonly cells: CellState[][];
+
+  constructor(
+    private readonly columnValues: number[][],
+    private readonly rowValues: number[][],
+  ) {
+    this.cells = Array.from({ length: rowValues.length }, () =>
+      Array.from({ length: columnValues.length }, () => CellState.EMPTY),
+    );
+  }
+
+  public get length(): number {
+    return this.columnValues.length;
+  }
+
+  public get width(): number {
+    return this.rowValues.length;
+  }
+
+  public get state(): CellState[][] {
+    return this.cells.map((row) => row.slice());
+  }
+}
---
+++ src/components/table/tests/table.test.ts
@@ -0,0 +1,45 @@
+import { describe, expect, it, beforeEach } from '@jest/globals';
+import { CellState } from '../../../contants/cell-state-enum';
+import { Table } from '../table';
+
+describe('table', () => {
+  describe('empty table', () => {
+    it('creates an empty table', () => {
+      const table = new Table([], []);
+      expect(table.length).toBe(0);
+      expect(table.width).toBe(0);
+    });
+  });
+
+  describe('5x5 snake table', () => {
+    let table: Table;
+
+    beforeEach(() => {
+      const rowValues = [[5], [1], [5], [1], [5]];
+      const collumnValues = [
+        [3, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 3],
+      ];
+      table = new Table(rowValues, collumnValues);
+    });
+
+    it('has 5 of length', () => {
+      expect(table.length).toBe(5);
+    });
+
+    it('has 5 of width', () => {
+      expect(table.length).toBe(5);
+    });
+
+    it('is empty initially', () => {
+      expect(table.state).toHaveLength(5);
+      table.state.forEach((row) => {
+        expect(row).toHaveLength(5);
+        row.map((val) => expect(val).toBe(CellState.EMPTY));
+      });
+    });
+  });
+});
