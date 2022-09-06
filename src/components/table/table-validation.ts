47238cc9a13a607cfc1128aa5b8228cd7019ff65
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-09-06 10:27:54 -0300

    refactor: extract

--- src/components/table/table-validation.ts
+++ src/components/table/table-validation.ts
@@ -2,13 +2,17 @@ export function validateRangeValues(
   linesValues: number[][],
   max: number,
 ): void {
-  linesValues.forEach((lineValues) => {
-    const total =
-      lineValues.reduce((acc, curr) => acc + curr, 0) + lineValues.length - 1;
-    if (total > max) {
-      throw new Error('Too many values in the line');
-    }
-  });
+  linesValues.forEach((lineValues) =>
+    validateRangeInLineValues(lineValues, max),
+  );
+}
+
+function validateRangeInLineValues(lineValues: number[], max: number): void {
+  const total =
+    lineValues.reduce((acc, curr) => acc + curr, 0) + lineValues.length - 1;
+  if (total > max) {
+    throw new Error('Too many values in the line');
+  }
 }
 
 export function validateNegativeValues(linesValues: number[][]): void {
