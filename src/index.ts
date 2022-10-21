9bec2441aff474eb42bc2221a75d38c12b88cbca
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-10-21 20:19:47 -0300

    setup index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-import { sayHi } from './modules';
+import { createInterface } from 'readline';
+import { tables } from './contants/table-definitions';
+import { ConsoleGame } from './components/console-game/console-game';
+import { ConsoleMenu } from './components/console-menu/console-menu';
+import { ConsolePrompter } from './components/console-menu/console-prompter';
 
-console.log(sayHi());
+const inter = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const prompter = new ConsolePrompter({
+  inter,
+});
+
+const game = new ConsoleGame({
+  consoleMenu: new ConsoleMenu({
+    prompter,
+  }),
+  log: console.log,
+  prompter,
+});
+
+game
+  .run(tables)
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
