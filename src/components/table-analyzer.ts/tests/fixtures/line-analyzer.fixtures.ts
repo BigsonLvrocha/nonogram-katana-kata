192faff5d96328f59583c192734099b969568b88
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-10-31 20:32:11 -0300

    add first case of line analyzer

---
+++ src/components/table-analyzer.ts/tests/fixtures/line-analyzer.fixtures.ts
@@ -0,0 +1,40 @@
+import { CellState } from '../../../../constants/cell-state-enum';
+import { LineValueState } from '../../line-analyzer';
+
+const u = CellState.UNKNOWN;
+
+interface DatasetEntry {
+  line: {
+    valueGroupsValues: number[];
+    cells: CellState[];
+  };
+  answer: {
+    cells: Array<Set<number>>;
+    valueGroupsState: LineValueState[];
+    valid: boolean[];
+  };
+}
+
+interface Dataset {
+  [key: string]: DatasetEntry;
+}
+
+export const dataset: Dataset = {
+  'Empty case [1]': {
+    line: {
+      valueGroupsValues: [1],
+      cells: [u, u, u, u, u],
+    },
+    answer: {
+      cells: [
+        new Set([0]),
+        new Set([0]),
+        new Set([0]),
+        new Set([0]),
+        new Set([0]),
+      ],
+      valueGroupsState: [LineValueState.EMPTY],
+      valid: [true, true, true, true, true],
+    },
+  },
+};
---
+++ src/components/table-analyzer.ts/tests/line-analyzer.test.ts
@@ -0,0 +1,34 @@
+import { expect, describe, it } from '@jest/globals';
+import { dataset } from './fixtures/line-analyzer.fixtures';
+import { analizeLine } from '../line-analyzer';
+
+describe('line-analyzer', () => {
+  Object.entries(dataset).forEach(([caseName, data]) => {
+    describe(caseName, () => {
+      const result = analizeLine(data.line.cells, data.line.valueGroupsValues);
+
+      it('results with the right number of cells', () => {
+        expect(result.cellIndexes.length).toEqual(data.answer.cells.length);
+        expect(result.valid.length).toEqual(data.answer.valid.length);
+      });
+
+      it('results with the right number of group value', () => {
+        expect(result.valuesState.length).toEqual(
+          data.answer.valueGroupsState.length,
+        );
+      });
+
+      data.answer.valid.forEach((valid, index) => {
+        it('results with the right valid', () => {
+          expect(result.valid[index]).toEqual(valid);
+        });
+      });
+
+      data.answer.cells.forEach((cells, index) => {
+        it('results with the correct options', () => {
+          expect(result.cellIndexes[index]).toEqual(cells);
+        });
+      });
+    });
+  });
+});
