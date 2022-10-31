497f282bd874b876426e1f16065f908cd41f20d1
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2022-10-31 20:38:36 -0300

    add another test case

--- src/components/table-analyzer.ts/tests/fixtures/line-analyzer.fixtures.ts
+++ src/components/table-analyzer.ts/tests/fixtures/line-analyzer.fixtures.ts
@@ -54,4 +54,21 @@ export const dataset: Dataset = {
       valid: [true, true, true, true, true],
     },
   },
+  'Empty case [1,1]': {
+    line: {
+      valueGroupsValues: [1, 1],
+      cells: [u, u, u, u, u],
+    },
+    answer: {
+      cells: [
+        new Set([0]),
+        new Set([0]),
+        new Set([0, 1]),
+        new Set([1]),
+        new Set([1]),
+      ],
+      valid: [true, true, true, true, true],
+      valueGroupsState: [LineValueState.EMPTY, LineValueState.EMPTY],
+    },
+  },
 };
